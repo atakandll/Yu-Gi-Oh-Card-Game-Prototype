134879ae2cb4ddb86040393a0495ebdd9c96acd8
atakandll <atakanccnar@outlook.com>
2024-02-02 20:29:08 +0300

    Singleton pattern setup

---
+++ Assets/Scripts/Patterns/Singleton/PersistentSingleton.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace Patterns.Singleton
+{
+    public class PersistentSingleton<T> : MonoBehaviour where T : Component
+    {
+        private static T instance;
+
+        #region Public Accessors
+
+        // Static instance of PersistentGameObjectSingleton which allows it to be accessed by any other script
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                    CreateInstance();
+                else
+                    HandleDuplication();
+
+                return instance;
+            }
+        }
+
+        #endregion
+        
+        private static void CreateInstance()
+        {
+            var go = new GameObject(typeof(T).ToString());
+            instance = go.AddComponent<T>();
+        }
+
+        private static void HandleDuplication()
+        {
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance.
+            var copies = FindObjectsOfType(typeof(T));
+            foreach (var copy in copies)
+                if (copy != instance)
+                    Destroy(copy);
+        }
+    }
+}
---
+++ Assets/Scripts/Patterns/Singleton/Singleton.cs
@@ -0,0 +1,25 @@
+﻿namespace Patterns.Singleton
+{
+    public class Singleton<T> where T : class, new()
+    {
+        //a protected constructor
+        protected Singleton()
+        {
+        }
+
+        //public getter
+        public static T Instance { get; private set; } = CreateInstance();
+
+        private static T CreateInstance()
+        {
+            return Instance ?? (Instance = new T());
+        }
+
+        //Setter used to inject an instance 
+        public void InjectInstance(T instance)
+        {
+            if (instance != null)
+                Instance = instance;
+        }
+    }
+}
