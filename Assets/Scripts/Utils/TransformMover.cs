b2e37c76bc7ffaa4d64e07c8af200125c48fd501
atakandll <atakanccnar@outlook.com>
2024-02-10 23:37:50 +0300

    Some utils added (TransformMover and UIStartEnable

---
+++ Assets/Scripts/Utils/TransformMover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Utils
+{
+    
+    public class TransformMover : MonoBehaviour
+    {
+        public void MoveUp()
+        {
+            transform.localPosition = new Vector3(0,1,0);
+        }
+
+        public void MoveDown()
+        {
+            transform.localPosition = new Vector3(0,-1,0);
+        }
+    }
+}
---
+++ Assets/Scripts/Utils/UIStartEnabler.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace Utils
+{
+    // Enable and Disable gameobject on start
+    public class UIStartEnabler : MonoBehaviour
+    {
+        public bool IsActive;
+
+        private void Start()
+        {
+            gameObject.SetActive(IsActive);
+        }
+    }
+}
