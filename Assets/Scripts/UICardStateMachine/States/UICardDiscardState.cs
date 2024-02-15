7f29af719d758acee35c02bf526e3b10274bac75
atakandll <atakanccnar@outlook.com>
2024-02-16 01:02:35 +0300

    Discard state implemented

--- Assets/Scripts/UICardStateMachine/States/UICardDiscardState.cs
+++ Assets/Scripts/UICardStateMachine/States/UICardDiscardState.cs
@@ -1,7 +1,37 @@
-﻿namespace UICardStateMachine.States
+﻿using Data;
+using Patterns.StateMachine;
+using UICardHand;
+using UnityEngine;
+
+namespace UICardStateMachine.States
 {
-    public class UICardDiscardState
+    public class UICardDiscardState : UIBaseCardState
     {
+        private Vector3 startScale { get; set; }
+
+        protected UICardDiscardState(IUICard handler, UICardParameters parameters, BaseStateMachine fsm) : base(handler, parameters, fsm)
+        {
+        }
         
+        
+
+        public override void OnEnterState()
+        {
+            Disable();
+            SetScale();
+            SetRotation();
+        }
+
+        private void SetScale()
+        {
+            var finalScale = Handler.Transform.localScale * Parameters.DiscardedSize;
+            Handler.ScaleTo(finalScale,Parameters.ScaleSpeed);
+        }
+
+        private void SetRotation()
+        {
+            var speed = Handler.IsPlayer ? Parameters.RotationSpeed : Parameters.RotationSpeedP2;
+            Handler.RotateTo(Vector3.zero, speed);
+        }
     }
 }
