6a8528954531f0a5e4591a2b1403644a98dbf8a0
atakandll <atakanccnar@outlook.com>
2024-02-19 07:48:44 +0300

    Draw state implemented

--- Assets/Scripts/UICardStateMachine/States/UICardDrawState.cs
+++ Assets/Scripts/UICardStateMachine/States/UICardDrawState.cs
@@ -1,7 +1,48 @@
-﻿namespace UICardStateMachine.States
+﻿using Data;
+using Patterns.StateMachine;
+using UICardHand;
+using UICardTransform;
+using UnityEngine;
+
+namespace UICardStateMachine.States
 {
-    public class UICardDrawState
+    // This state draw the collider of the card
+    public class UICardDrawState : UIBaseCardState
     {
-        
+        private Vector3 startScale { get; set; }
+        protected UICardDrawState(IUICard handler, UICardParameters parameters, BaseStateMachine fsm) : base(handler, parameters, fsm)
+        {
+        }
+
+        public override void OnEnterState()
+        {
+            CachePreviousScale();
+            DisableCollision();
+            SetScale();
+            Handler.Movement.OnFinishMotion += GoToIdle;
+        }
+
+        public override void OnExitState()
+        {
+            Handler.Movement.OnFinishMotion -= GoToIdle;
+        }
+
+        private void GoToIdle()
+        {
+            Handler.Enable();
+        }
+
+        private void CachePreviousScale()
+        {
+            var localScale = Handler.Transform.localScale;
+            startScale = localScale;
+            localScale *= Parameters.StartSizeWhenDraw;
+            Handler.Transform.localScale = localScale;
+        }
+
+        private void SetScale()
+        {
+            Handler.ScaleTo(startScale,Parameters.ScaleSpeed);
+        }
     }
 }
